41a5764b8558c5f269a820ef007f05131f357c1b
sh0608 <85568140+sh0608@users.noreply.github.com>
2021-06-14 08:12:03 -0400

    Initial Commit

---
+++ GitXcode/GitXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitXcodeApp.swift
+//  GitXcode
+//
+//  Created by Wai Kwong Ho on 6/14/21.
+//
+
+import SwiftUI
+
+@main
+struct GitXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
